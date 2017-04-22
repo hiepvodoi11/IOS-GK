9cf8b4338b362fe4da8e5ccdd19082db4adeae2e
haiconga <haiconga@haicongas-Mac.local>
2017-04-22 11:49:27 +0700

    Initial Commit

---
+++ GK_NhaHang/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GK_NhaHang
+//
+//  Created by haiconga on 4/22/17.
+//  Copyright © 2017 haiconga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
