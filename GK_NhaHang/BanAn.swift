74015455db8e02aa8436449f82254953b5ed6f7a
haiconga <haiconga@haicongas-Mac.local>
2017-04-24 22:39:28 +0700

    added

---
+++ GK_NhaHang/BanAn.swift
@@ -0,0 +1,18 @@
+//
+//  NhaHang.swift
+//  GK_NhaHang
+//
+//  Created by haiconga on 4/24/17.
+//  Copyright © 2017 haiconga. All rights reserved.
+//
+
+import Foundation
+class BanAn{
+    var maBan : Int?
+    var maKhuVuc : Int?
+    var hinhAnh : String?
+    var STTBan : Int?
+    var thongTin : String?
+    init() {
+    }
+}
